696755353547745f9fbe33fa55fedc14231be0eb
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2021-03-17 16:19:01 -0500

    postman framework

--- ci_tools/run_postman.sh
+++ ci_tools/run_postman.sh
@@ -15,6 +15,7 @@ createGlobalVarsPostman
 k8sFile="${1}"
 JOB_NAME="${2}"
 COLLECTION_ID="${3}"
+WAIT=${4:-50}
 export JOB_NAME COLLECTION_ID
 test ! -f "${k8sFile}" && echo "${k8sFile} - file not found" && exit 1
 envsubst < "${k8sFile}" > "${k8sFile}.final"
@@ -24,20 +25,20 @@ kubectl delete -f "${k8sFileFinal}" --ignore-not-found --force --grace-period=0 
 
 kubectl apply -f "${k8sFileFinal}" -n "${K8S_NAMESPACE}"
 
-timeout=50
-echo "waiting ${timeout}s for ${JOB_NAME} to complete "
+echo "waiting ${WAIT}s for ${JOB_NAME} to complete "
 while true ; do
   status="$(kubectl get job ${JOB_NAME} -o jsonpath='{.status.conditions[0].type}' --ignore-not-found -n ${K8S_NAMESPACE})"
   if test "${status}" = "Complete" -o "${status}" = "Failed" ; then
     break
   fi
   sleep 1
-  timeout=$((timeout-1))
-  test $timeout -le 1 && echo "JOB TIMED OUT" && break
+  WAIT=$((WAIT-1))
+  test $WAIT -le 1 && echo "JOB TIMED OUT" && break
 done
 
 kubectl logs "job/${JOB_NAME}" -n "${K8S_NAMESPACE}"
 
 echo "Job Status: ${status}"
 kubectl delete -f "${k8sFileFinal}" --ignore-not-found -n "${K8S_NAMESPACE}"
+rm "${k8sFileFinal}"
 test "${status}" = "Complete" && exit 0
